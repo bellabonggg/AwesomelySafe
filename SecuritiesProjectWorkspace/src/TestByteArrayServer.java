c54ed0caf6cef372ef92910deaebc76278928250
Jia Hao <jiahaog@gmail.com>
2015-04-17 13:11:51 +0800

    Added awesome sockets test code

---
+++ SecuritiesProjectWorkspace/src/TestByteArrayServer.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by JiaHao on 17/4/15.
+ */
+public class TestByteArrayServer {
+
+
+    public static void main(String[] args) throws IOException {
+
+        AwesomeServerSocket serverSocket = new AwesomeServerSocket(5555);
+
+        serverSocket.acceptClient();
+        byte[] readMessage = serverSocket.readByteArrayForClient(0);
+
+
+        System.out.println(Arrays.toString(readMessage));
+
+    }
+
+
+}
