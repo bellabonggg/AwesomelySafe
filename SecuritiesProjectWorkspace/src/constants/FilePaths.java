784f5083f3f0c4d43bc905bcf0c8fbf6af3f805e
Qin-Cheng <qincheng_peh@mymail.sutd.edu.sg>
2015-04-21 11:31:51 +0800

    hi

--- SecuritiesProjectWorkspace/src/constants/FilePaths.java
+++ SecuritiesProjectWorkspace/src/constants/FilePaths.java
@@ -5,9 +5,9 @@ package constants;
  */
 public class FilePaths {
 
-    public static String SERVER_PRIVATE_KEY = "src/keys/privateServer.der";
-    public static String SERVER_PUBLIC_KEY = "src/keys/publicServer.der";
-    public static String CA_CERTIFICATE = "src/keys/CA.crt";
-    public static String SERVER_CERTIFICATE = "src/keys/server.crt";
+    public static String SERVER_PRIVATE_KEY = "src\\keys\\privateServer.der";
+    public static String SERVER_PUBLIC_KEY = "src\\keys\\publicServer.der";
+    public static String CA_CERTIFICATE = "src\\keys\\CA.crt";
+    public static String SERVER_CERTIFICATE = "src\\keys\\server.crt";
 
 }
